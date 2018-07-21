1281944b13ba22c465b8e72df62625325a1cb97c
roman <shmoop207@gmail.com>
2018-07-21 15:30:39 +0300

    added exports to index

--- index.js
+++ index.js
@@ -2,4 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var queue_1 = require("./lib/queue");
 exports.Queue = queue_1.Queue;
+var job_1 = require("./lib/job");
+exports.Job = job_1.Job;
+var events_1 = require("./lib/events");
+exports.Events = events_1.Events;
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,2 +1,6 @@
+
 export {Queue} from './lib/queue'
+export {Job} from './lib/job'
+export {Events} from './lib/events'
+export {IOptions,IJobOptions,IHandlerOptions} from './lib/IOptions'
 
